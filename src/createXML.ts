309f91c0426bebb99e73f3908963dd9bcac010ce
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-18 11:09:11 +0900

    clean

--- src/createXML.ts
+++ src/createXML.ts
@@ -1,5 +1,11 @@
 import { searchResult } from './twitter'
 
+/**
+ * XMLを作成
+ *
+ * @param items 検索結果
+ * @returns XML
+ */
 export const createXML = (items: searchResult[]) => {
   const output = HtmlService.createTemplateFromFile('template')
   output.items = items

2a8295a188465ac18455042c019201355c1e247a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-07 02:36:51 +0900

    clean

--- src/createXML.js
+++ src/createXML.js
@@ -4,7 +4,7 @@
  * @param items 検索結果
  * @returns XML
  */
-function createXML (items)  {
+function createXML(items) {
   const output = HtmlService.createTemplateFromFile('template')
   output.items = items
 
